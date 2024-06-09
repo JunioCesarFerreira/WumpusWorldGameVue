c44ec52d31b8ee34d5a3218360886eb2a27b9bba
Junio Cesar Ferreira <jcf_ssp@hotmail.com>
2024-06-09 14:13:18 -0300

    review - Images and overlay image

--- WumpusWorldGame/src/components/GameGrid.vue
+++ WumpusWorldGame/src/components/GameGrid.vue
@@ -3,7 +3,8 @@
     <div v-for="(row, rowIndex) in grid" :key="rowIndex" class="row">
       <div v-for="cell in row" :key="`${cell.id[0]}-${cell.id[1]}`" class="col p-1">
         <div class="cell">
-          <img :src="getImgForCell(cell.id)" alt="Cell Image">
+          <img :src="getImgForCell(cell.id)" alt="Cell Image" class="base-image">
+          <img v-if="shouldOverlayPlayer(cell.id)" :src="getPlayerImg()" alt="Player Image" class="overlay-image">
           <div class="overlay-text">{{ getWarningTextForCell(cell.id) }}</div>
         </div>
       </div>
@@ -30,12 +31,12 @@ interface Cell {
 }
 
 export default defineComponent({
-  props:{
+  props: {
     directionProps: {
       type: Number as () => Direction,
       required: true
     },
-    playerPositionProps:{
+    playerPositionProps: {
       type: Object as PropType<Position>,
       required: true
     },
@@ -58,72 +59,74 @@ export default defineComponent({
         [{ id: [1, 1] }, { id: [2, 1] }, { id: [3, 1] }, { id: [4, 1] }],
       ] as Cell[][],
       direction: Direction.Down,
-      wumpusPosition: [2,3] as Position,
-      pitsPositions: [[1,4],[3,1],[4,3]] as Position[],
-      goldPosition: [4,4] as Position,
-      visitedCells: [[1,1]] as Position[] // Lista para armazenar as células visitadas
+      wumpusPosition: [2, 3] as Position,
+      pitsPositions: [[1, 4], [3, 1], [4, 3]] as Position[],
+      goldPosition: [4, 4] as Position,
+      visitedCells: [[1, 1]] as Position[] // Lista para armazenar as células visitadas
     };
   },
   methods: {
     getImgForCell(pos: Position) {
-      if (arePositionsEqual(pos, this.playerPositionProps)) {
-        switch (this.direction) {
-            case Direction.Up:
-                return player_up_img
-            case Direction.Down:
-                return player_down_img
-            case Direction.Left:
-                return player_left_img
-            case Direction.Right:
-                return player_right_img
-        }
-      } else if (this.showProps) {
-        if (arePositionsEqual(pos, this.wumpusPosition)){
+      if (this.showProps || isPositionInArray(pos, this.visitedCells)) {  
+        if (arePositionsEqual(pos, this.wumpusPosition)) {
           return wumpus_img;
-        } else if (arePositionsEqual(pos, this.goldPosition)){
+        } else if (arePositionsEqual(pos, this.goldPosition)) {
           return gold_img;
-        } else if (isPositionInArray(pos, this.pitsPositions)){
+        } else if (isPositionInArray(pos, this.pitsPositions)) {
           return black_block_img;
         } else {
-          return transparent_block_img; 
+          return transparent_block_img;
         }
       } else {
-        if (isPositionInArray(pos, this.visitedCells)){
-          return transparent_block_img;
-        }else {
-          return brown_block_img;
-        }
+        return brown_block_img;
+      }
+    },
+    shouldOverlayPlayer(pos: Position) {
+      return arePositionsEqual(pos, this.playerPositionProps);
+    },
+    getPlayerImg() {
+      switch (this.direction) {
+        case Direction.Up:
+          return player_up_img;
+        case Direction.Down:
+          return player_down_img;
+        case Direction.Left:
+          return player_left_img;
+        case Direction.Right:
+          return player_right_img;
+        default:
+          return player_down_img;
       }
     },
     getWarningTextForCell(id: Position) {
       return "";
     }
   },
-  watch:{
-    directionProps:{
-      handler(newValue: Direction){
-        console.log('watch directionProps:', newValue)
-        this.direction = newValue
+  watch: {
+    directionProps: {
+      handler(newValue: Direction) {
+        console.log('watch directionProps:', newValue);
+        this.direction = newValue;
       }
     },
-    playerPositionProps:{
-      handler(newValue: Position){
-        console.log("watch playerPositionProps visitedCells", this.visitedCells)
+    playerPositionProps: {
+      handler(newValue: Position) {
+        console.log("watch playerPositionProps visitedCells", this.visitedCells);
         // Atualiza a lista de células visitadas quando a posição do jogador mudar
         if (!isPositionInArray(newValue, this.visitedCells)) {
-          const pos = {...newValue} as Position;
+          const pos = { ...newValue } as Position;
           this.visitedCells.push(pos);
         }
-        console.log("watch playerPositionProps visitedCells", this.visitedCells)
+        console.log("watch playerPositionProps visitedCells", this.visitedCells);
       },
       deep: true
     },
-    gameProps:{
-      handler(newValue: Game){
-        console.log('watch gameProps:',newValue)
-        this.wumpusPosition = newValue.WumpusPosition
-        this.pitsPosition = newValue.PitsPositions
-        this.goldPosition = newValue.GoldPosition
+    gameProps: {
+      handler(newValue: Game) {
+        console.log('watch gameProps:', newValue);
+        this.wumpusPosition = newValue.WumpusPosition;
+        this.pitsPositions = newValue.PitsPositions;
+        this.goldPosition = newValue.GoldPosition;
       }
     }
   }
@@ -156,11 +159,23 @@ export default defineComponent({
   height: 80px; /* Define a altura fixa */
   object-fit: scale-down; /* Ajusta a imagem para cobrir completamente o espaço, preservando a proporção */
 }
+.base-image {
+  position: relative;
+  z-index: 1;
+}
+.overlay-image {
+  position: absolute;
+  width: 80px;
+  height: 80px;
+  object-fit: scale-down;
+  z-index: 2;
+}
 .overlay-text {
   position: absolute;
   color: white;
   font-size: 16px;
   font-weight: bold;
   text-shadow: 1px 1px 2px black;
+  z-index: 3;
 }
 </style>
